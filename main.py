1d6519b77205c28ee7e5967c235f34bb30eb10e6
Ben L <55718887+jaymay284@users.noreply.github.com>
2022-09-27 09:59:21 -0600

    Database Creation, Global Variables cleaned up.

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import sqlite3
+from os.path import exists
+
+credentials = open("credentials.txt", "r")
+#TODO process keys and tokens
+credentials.close()
 
 def main():
-    makeDB()
+    #if the database does not exist, we must make it, otherwise do nothing
+    if(not exists("flights.db")):
+        makeDB("flights.db")
+    else: print("The Database already exists, skipping database creation.")
 
-def makeDB():
-    con = sqlite3.connect("tutorial.db")
+def makeDB(fName):
+    #connect to and initialize the database file
+    con = sqlite3.connect(fName)
     cur = con.cursor()
-    cur.execute("CREATE TABLE `Flight` (`FlightID` INT,PRIMARY KEY (`FlightID`));");
+    cur.execute("CREATE TABLE `Flight` (`FlightID` INT,PRIMARY KEY (`FlightID`));")
+    con.close()
 
 if(__name__ == "__main__"):
     main()
