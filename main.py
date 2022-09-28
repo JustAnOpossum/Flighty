a19701df245e3baa4191bed145e7fd92b22c9eec
Ben L <55718887+jaymay284@users.noreply.github.com>
2022-09-28 11:22:33 -0600

    Updated MakeDB
    Proper Error Handling, Tested Value insertion and querying.

--- main.py
+++ main.py
@@ -7,16 +7,47 @@ credentials.close()
 
 def main():
     #if the database does not exist, we must make it, otherwise do nothing
-    if(not exists("flights.db")):
-        makeDB("flights.db")
+    if(not exists("flighty.db")):
+        makeDB("flighty.db")
     else: print("The Database already exists, skipping database creation.")
 
 def makeDB(fName):
-    #connect to and initialize the database file
-    con = sqlite3.connect(fName)
-    cur = con.cursor()
-    cur.execute("CREATE TABLE `Flight` (`FlightID` INT,PRIMARY KEY (`FlightID`));")
-    con.close()
+    try:    
+        #connect to and initialize the database file
+        con = sqlite3.connect(fName)
+        cur = con.cursor()
+        #generate schema for Session
+        cur.execute("""
+            CREATE TABLE `Session` (
+                `UserID` INT NOT NULL,
+                `MessageID` INT,
+                `NumFlights` INT NOT NULL,
+                PRIMARY KEY (`UserID`)
+            );
+        """)
+
+        #Generate schema for Flight
+        cur.execute("""
+            CREATE TABLE `Flight` (
+                `UserID` VARCHAR NOT NULL,
+                `InternalFlightID` INT NOT NULL,
+                `Delay` INT NOT NULL,
+                `DepartureTime` VARCHAR NOT NULL,
+                `SchedArrivalTime` VARCHAR NOT NULL,
+                `DepartTerminal` VARCHAR NOT NULL,
+                `DepartGate` VARCHAR NOT NULL,
+                `ArriveTerminal` VARCHAR NOT NULL,
+                `ArriveGate` VARCHAR NOT NULL,
+                `ArriveAPC` VARCHAR NOT NULL,
+                `DepartAPC` VARCHAR NOT NULL,
+                `Coords` VARCHAR NOT NULL
+            );
+        """)
+        #close the connection
+        con.close()
+    #exception error handling    
+    except sqlite3.Error as er:
+        print(er + " in MakeDB()")
 
 if(__name__ == "__main__"):
     main()
