e96ea68a41f57163a6ca27fa89ad9e12d25f3c36
Ben L <55718887+jaymay284@users.noreply.github.com>
2022-10-06 16:22:23 -0600

    Demo 1 Finished Product
    Runs completely in CLI. User can enter any active flight and receive it's updated information.
    
    Database Now Works as intended. lol.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sqlite3
 from os.path import exists
+from typing import List
 from flightTracking import *
 from credentials import *
 
@@ -31,15 +32,63 @@ def main():
             print("Flight may not exist. Try again.")
             continue
         # if flight does exist, print the response / json
+        if(isinstance(flightData, List)):
+            flightData = flightData[0]
+                
+        print(type(flightData))
         print(flightData)
+            
+        flightID = str(flightData["flightID"])
+        flightDelay = str(flightData["Delay"])
+        flightDepTime = str(flightData["DepTime"])
+        flightArvTime = str(flightData["ArvTime"])
+        flightDepTerm = str(flightData["DepTerm"])
+        flightDepGate = str(flightData["DepGate"])
+        flightArvTerm = str(flightData["ArvTerm"])
+        flightArvGate = str(flightData["ArvGate"])
+        flightArvCode = str(flightData["ArvCode"])
+        flightDepCode = str(flightData["DepCode"])
+        flightRegistration = str(flightData["Registration"])
+
+        print("Flight ID: " + flightID)
+        print("FLight Delay: " + flightDelay)
+        print("Flight Departure Time: " + flightDepTime)
+        print("Flight Arrival Time: " + flightArvTime)
+        print("Flight Departure Terminal: " + flightDepTerm)
+        print("Flight Departure Gate: " + flightDepGate)
+        print("Flight Arrival Terminal: " + flightArvTerm)
+        print("Flight Arrival Gate: " + flightArvGate)
+        print("Arrival Airport Code: " + flightArvCode)
+        print("Departing Airport Code: " + flightDepCode)
+        print("Flight Registration Code: " + flightRegistration)
+
         # add this data to database
         try:
             # connect to the database
             con = sqlite3.connect("flighty.db")
             cur = con.cursor()
+            data = (
+            "1", 
+            1, 
+            int(flightDelay), 
+            flightDepTime, 
+            flightArvTime, 
+            flightDepTerm, 
+            flightDepGate, 
+            flightArvTerm, 
+            flightArvGate, 
+            flightArvCode, 
+            flightDepCode, 
+            "Coordinates not implemented yet!", 
+            flightRegistration
+            )
+            cur.execute("INSERT INTO Flight VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data)
+            #commit issues into flighty.db
+            con.commit()
+
             con.close()
         except sqlite3.Error as er:
-            print(er + " in main()")
+            print(er)
         # get new user input
         userInput = input("Enter q to quit, press enter to continue")
     return
@@ -67,6 +116,14 @@ def makeDB(fName):
                 PRIMARY KEY (`UserID`)
             );
         """)
+        #FIXME REMOVE THIS AFTER DEMO 1
+        cur.execute(
+            """
+            INSERT INTO Session VALUES (1, 1, 1), (2, 2, 2), (3, 3, 3);
+            """
+        )
+        #FIXME REMOVE THIS ONE TOO AFTER DEMO 1
+        con.commit()
 
         # Generate schema for Flight
         cur.execute("""
@@ -82,7 +139,8 @@ def makeDB(fName):
                 `ArriveGate` VARCHAR NOT NULL,
                 `ArriveAPC` VARCHAR NOT NULL,
                 `DepartAPC` VARCHAR NOT NULL,
-                `Coords` VARCHAR NOT NULL
+                `Coords` VARCHAR NOT NULL,
+                `Registration` VARCHAR NOT NULL
             );
         """)
         # close the connection
