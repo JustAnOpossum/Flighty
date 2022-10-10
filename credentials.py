b7672d50c1553664c3eae994744c6c57be059f44
Cameron <cameronclapp11@gmail.com>
2022-10-10 11:00:15 -0600

    Fixed configFile

--- credentials.py
+++ credentials.py
@@ -1,15 +1,28 @@
 import configparser
+from os.path import exists
 
 keys = configparser.ConfigParser()
+isLoaded = False
 
-#Loads API keys from file
+# Loads API keys from file
+
+
 def loadKeys(configFile):
+    if (not exists(configFile)):
+        print("No credentials file found")
+        return
+    else:
+        print("Loading keys...")
+        loadKeys(configFile)
+        isLoaded = True
     keys.read(configFile)
 
-#Gets a key from the config file
+# Gets a key from the config file
+
+
 def getKey(keyName):
-    try:
-        return keys['KEYS'][keyName]
-    except:
-        return ""
-    
+    if isLoaded:
+        try:
+            return keys['KEYS'][keyName]
+        except:
+            return ""
