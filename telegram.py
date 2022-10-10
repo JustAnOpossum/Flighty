54008f3e29a3a6c8917d27384322071680630efe
Cameron <cameronclapp11@gmail.com>
2022-10-10 11:30:11 -0600

    Added basic telegram support

--- telegram.py
+++ telegram.py
@@ -1,6 +1,22 @@
-# from telegram import Update
-# from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-
 from credentials import *
-loadKeys("credentials.txt")
-print(getKey("Discord"))
+import telebot
+
+
+def main():
+    loadKeys("credentials.txt")
+    bot = telebot.TeleBot(getKey("Telegram"))
+
+    @bot.message_handler(commands=['start', 'help'])
+    def send_welcome(message):
+        bot.reply_to(message, "Welcome to the bot")
+
+    @bot.message_handler(commands=['trackFlight'])
+    def trackFlight(message):
+        bot.reply_to(message, "Testing123")
+
+    print("Bot Loaded")
+    bot.infinity_polling()
+
+
+if (__name__ == "__main__"):
+    main()
