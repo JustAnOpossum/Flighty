3ae9bffb0a3396faf8baf2c55fb2c59d4c0525fd
Cameron <cameronclapp11@gmail.com>
2022-10-10 11:07:20 -0600

    Fixed credetntials

--- credentials.py
+++ credentials.py
@@ -2,7 +2,6 @@ import configparser
 from os.path import exists
 
 keys = configparser.ConfigParser()
-isLoaded = False
 
 # Loads API keys from file
 
@@ -13,15 +12,13 @@ def loadKeys(configFile):
         return
     else:
         print("Loading keys...")
-        isLoaded = True
         keys.read(configFile)
 
 # Gets a key from the config file
 
 
 def getKey(keyName):
-    if isLoaded:
-        try:
-            return keys['KEYS'][keyName]
-        except:
-            return ""
+    try:
+        return keys['KEYS'][keyName]
+    except:
+        return ""
--- telegram.py
+++ telegram.py
@@ -1,2 +1,6 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+# from telegram import Update
+# from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+from credentials import *
+loadKeys("credentials.txt")
+print(getKey("Discord"))
