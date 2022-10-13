8b1a7e3088a49bb5fe046db2ab191f566896bbdb
Ben L <blongwell284@gmail.com>
2022-10-13 15:04:12 -0600

    Update flightyDiscord.py
    Basic Flight Information Delivery.
    Needs: User Flight Selection
    Needs: Edit Message every X Minutes.

--- flightyDiscord.py
+++ flightyDiscord.py
@@ -1,21 +1,46 @@
+from urllib import response
 import discord
 from discord.ext import commands
 from backend.credentials import *
+from backend.flightTracking import *
+from backend.credentials import *
+from backend.database import *
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix=">", intents=intents)
-
+bot = commands.Bot(intents=intents)
 
 @bot.slash_command(name="track_flight")
-async def track_flight(ctx):
-    await ctx.respond("Tracking Flight!")
+async def track_flight(ctx, flight_code:discord.Option(str)):
+    try:
+        print("Getting flight data!")
+        flightData = getFlight(flight_code)
+    except flightData as er:
+        print(er)
+    #if the flight does not exist, return an error embed
+    if(flightData == {} or flightData == None):
+        myEmbed = discord.Embed(title="Flight Tracker", description="You entered a flight code that does not coorespond to an active flight. Please try again.", color=0xFF0000)
+        await ctx.respond(embed=myEmbed)
+    else:
+        #check if multiple flights exist
+        print(type(flightData))
+        print(len(flightData))
+        if(len(flightData) > 1):
+            print("There are more than one flights with that flight code. We must ask the user which flight they are referring to.")
+            for flight in flightData:
+                myEmbed = discord.Embed(title=f"Flight Tracker: {flight_code}", description=str(flight))
+                await ctx.respond(embed=myEmbed)
+        #if more than 1 exists in the API call, ask the user which flight time they are referring to
 
+        #show the user their accurate flight information.
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
+        #here we are certain the flight exists, we serve the data to the user.
+        #myEmbed = discord.Embed(title=f"Flight Tracker: {flight_code}", description=str(flightData))
+        #await ctx.respond(embed=myEmbed)
 
-loadKeys("backend/credentials.txt")
-print(getKey("Discord"))
-bot.run(getKey("Discord"))
+def main():
+    loadKeys("backend/credentials.txt")
+    bot.run(getKey("Discord"))
+
+if (__name__ == "__main__"):
+    main()
